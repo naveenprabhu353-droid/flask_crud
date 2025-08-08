097a6be27e5be64b2a8dbb03276252e30991b913
naveenprabhu353-droid <naveenprabhu353@gmail.com>
2025-08-08 19:28:20 +0530

    function

---
+++ Function.py
@@ -0,0 +1,19 @@
+def add():
+    print("Addition")
+    a=int(input("Enter a value:"))
+    b=int(input("Enter B value:"))
+    print("Value:",a+b)
+def sub():
+    print("subtraction")
+    a=int(input("Enter a value:"))
+    b=int(input("Enter b value:"))
+    print("value:",a-b)
+def mul():
+    print("Multiplication")
+    a=int(input("Enter a value :"))
+    b=int(input("Enter b value:"))
+    print(a*b)
+    
+add()
+sub()
+mul()
